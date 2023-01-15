2084c6bf8114dd56731cdda86a44d6408b2a7f96
Alla <alla_freeart@mail.ru>
2023-01-15 20:49:09 +0400

    Example005_Condition_If

---
+++ Example005_Condition_If/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите свое имя ");
+string username = Console.ReadLine();
+if (username == "Маша")
+{
+  Console.WriteLine ("Ура, это Маша");  
+}
+else
+{
+Console.Write("Привет, ");  
+Console.WriteLine(username);
+}
+
+
+
